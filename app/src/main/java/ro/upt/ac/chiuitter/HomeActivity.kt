aa8342979219fb9a821b4491d59c7bb4c45e868a
Alexian Hentiu <alexianhentiu1208@gmail.com>
2024-10-30 16:21:08 +0200

    change itemsIndexed to items

--- app/src/main/java/ro/upt/ac/chiuitter/HomeActivity.kt
+++ app/src/main/java/ro/upt/ac/chiuitter/HomeActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Send
@@ -22,16 +22,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.app.ShareCompat
 
 class HomeActivity : AppCompatActivity() {
 
@@ -58,7 +55,7 @@ class HomeActivity : AppCompatActivity() {
                 // TODO 8: Make use of Compose DSL to describe the content of the list and make sure
                 // to instantiate a [ChiuitListItem] for every item in [chiuitListState.value].
                 LazyColumn {
-                    itemsIndexed(chiuitListState.value) { _, chiuit ->
+                    items(chiuitListState.value) { chiuit ->
                         ChiuitListItem(chiuit)
                     }
                 }
